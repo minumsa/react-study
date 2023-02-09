b27913c99f5c582315669b1e687fca3fbdaf645a
유일 <83910706+minumsa@users.noreply.github.com>
2023-02-09 14:00:13 +0900

    useEffect 기초 연습

--- src/Effect.js
+++ src/Effect.js
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function useEffect() {
-  return <div>useEffect 연습</div>;
+function Effect() {
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+
+  const handleInputChange = e => {
+    setName(e.target.value);
+  };
+
+  // 처음 마운팅될 때만 실행됨
+  useEffect(() => {
+    console.log("마운팅 👔");
+  }, []);
+
+  // // 렌더링될 때마다 실행됨
+  // useEffect(() => {
+  //   console.log("렌더링 🎨");
+  // });
+
+  // // 마운트 + count가 변경될 때만 실행됨
+  // useEffect(() => {
+  //   console.log("💯 count 변화");
+  // }, [count]);
+
+  // // 마운트 + name이 변경될 때만 실행됨
+  // useEffect(() => {
+  //   console.log("⭐️ name 변화");
+  // }, [name]);
+
+  return (
+    <div>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count: {count}</span>
+      <br />
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>name: {name}</span>
+    </div>
+  );
 }
 
-export default useEffect;
+export default Effect;
